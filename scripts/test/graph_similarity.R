cbbfee391554162b729e32bfa20639a84a46b706
Conor Tompkins <conorotompkins@gmail.com>
2021-12-27 15:39:23 -0500

    updated test script

--- scripts/test/graph_similarity.R
+++ scripts/test/graph_similarity.R
@@ -26,33 +26,117 @@ pa_shape_moll <- pa_shape %>%
   st_transform(mollweide)
 
 #load similarity index data
-similarity_index <- read_csv("data/big/similarity_index.csv") #%>% 
-  # rename(geo_id_reference = geo_id_1,
-  #        geo_id_compare = geo_id_2)
+similarity_index <- read_csv("data/big/similarity_index.csv")
+
+select_grid_id <- 26
 
 #check that each geo_id occurs 99 times
-similarity_grid <- prep_similarity_grid(similarity_index, 30) %>% 
-  mutate(highlight_grid = grid_id_reference == grid_id_compare,
-         highlight_grid = as.factor(highlight_grid))
+# similarity_grid <- prep_similarity_grid(similarity_index, 30) %>% 
+#   mutate(highlight_grid = grid_id_reference == grid_id_compare,
+#          highlight_grid = as.factor(highlight_grid))
 
-similarity_grid %>% 
+
+similarity_grid <- similarity_index %>% 
+  rename(geo_id_reference = geo_id_1,
+         geo_id_compare = geo_id_2) 
+
+#create IDs for each geo_id
+geo_id_index <- similarity_grid %>% 
+  select(geo_id_reference, geo_id_compare) %>% 
+  pivot_longer(cols = everything(),
+               names_to = "type", values_to = "geo_id") %>% 
+  arrange(geo_id) %>% 
+  distinct(geo_id) %>% 
+  separate(geo_id, into = c("x", "y"), sep = "_", remove = F) %>% 
+  mutate(across(.cols = c(x, y), as.numeric)) %>% 
+  arrange(x, y) %>% 
+  mutate(grid_id = row_number())
+
+geo_id_index %>%
+  ggplot(aes(x, y, label = grid_id)) +
+  geom_label()
+
+geo_id_index <- geo_id_index %>% 
+  select(-c(x, y))
+
+#choose one grid_id to test
+reference_coords <- geo_id_index %>% 
+  filter(grid_id == select_grid_id) %>% 
+  select(geo_id)
+
+similarity_geo <- similarity_grid %>% 
+  #join comparison geo_ids to get their grid_id
+  left_join(geo_id_index, by = c("geo_id_compare" = "geo_id")) %>% 
+  rename(grid_id_compare = grid_id) %>% 
+  #filter on the reference geo_id
+  filter(geo_id_reference == pull(reference_coords)) %>% 
+  separate(geo_id_compare, into = c("x_compare", "y_compare"), sep = "_")
+
+similarity_geo
+
+#turn reference x,y into sf coordinates
+reference_coords <- reference_coords %>% 
+  separate(geo_id, into = c("x", "y"), sep = "_") %>% 
+  mutate(across(everything(), as.numeric)) %>% 
+  st_as_sf(coords = c("x", "y"), crs = mollweide)
+
+similarity_geo %>% 
+  #turn compare x,y into sf coordinates
+  mutate(across(.cols = c(x_compare, y_compare), as.numeric)) %>% 
+  ggplot(aes(x_compare, y_compare)) +
+  geom_point()
+
+similarity_geo <- similarity_geo %>% 
+  #turn compare x,y into sf coordinates
+  mutate(across(.cols = c(x_compare, y_compare), as.numeric)) %>% 
+  st_as_sf(coords = c("x_compare", "y_compare"), crs = mollweide) %>%
+  #join to get grid_id of reference coords
+  left_join(geo_id_index, by = c("geo_id_reference" = "geo_id")) %>% 
+  rename(grid_id_reference = grid_id) %>% 
+  #reorder variables
+  select(geo_id_reference, grid_id_reference, geometry, grid_id_compare, distance)
+
+similarity_geo <- similarity_geo %>% 
+  mutate(centroid = st_point_on_surface(geometry)) %>% 
+  mutate(x = map_dbl(centroid, 1),
+         y = map_dbl(centroid, 2))
+
+similarity_geo <- similarity_geo %>% 
+  mutate(highlight_grid = grid_id_compare == select_grid_id)
+
+similarity_geo %>% 
+  filter(highlight_grid == T)
+
+similarity_geo %>% 
+  count(grid_id_reference) %>% 
+  distinct(n)
+
+similarity_geo %>% 
+  ggplot(aes(x, y)) +
+  geom_raster(aes(fill = distance)) +
+  scale_fill_viridis_c()
+
+similarity_geo %>% 
   st_drop_geometry() %>% 
   count(grid_id_reference) %>% 
   distinct(n)
 
-similarity_grid %>% 
+similarity_geo %>% 
   st_drop_geometry() %>% 
   count(grid_id_compare) %>% 
   distinct(n)
 
-similarity_grid %>% 
+similarity_geo %>% 
   ggplot() +
-  geom_sf(aes(fill = distance, size = highlight_grid), color = "white") +
-  scale_size_manual(values = c(0, .75)) +
+  geom_tile(aes(x, y, fill =  distance)) +
+  geom_sf(data = pa_shape_moll, alpha = 0) +
+  geom_point(data = filter(similarity_geo, highlight_grid == T),
+             aes(x, y), 
+             color = "white") +
   scale_fill_viridis_c(direction = 1) +
   labs(fill = "Distance")
 
-similarity_grid %>% 
+similarity_geo %>% 
   st_transform(crs = "EPSG:4326") %>% 
   mapdeck() %>% 
   add_sf(fill_colour = "distance",
@@ -60,24 +144,26 @@ similarity_grid %>%
          stroke_colour = "highlight_grid",
          legend = T,
          auto_highlight = T,
-         tooltip = "grid_id_compare")
+         tooltip = "grid_id_compare",
+         radius = 10^4)
 
 pal <- colorNumeric(
   palette = "viridis",
-  domain = similarity_grid$distance)
+  domain = similarity_geo$distance)
 
-similarity_grid %>% 
+similarity_geo %>% 
   mutate(grid_opacity = case_when(highlight_grid == "TRUE" ~ .9,
-                                  highlight_grid == "FALSE" ~ .6)) %>% 
+                                  highlight_grid == "FALSE" ~ .6)) %>%
   st_transform(crs = "EPSG:4326") %>% 
   leaflet() %>%
   addProviderTiles(providers$Stamen.TonerLite,
                    options = providerTileOptions(noWrap = TRUE)) %>%
-  addPolygons(layerId = ~geometry,
-              fillColor = ~pal(distance),
-              fillOpacity = ~grid_opacity,
-              stroke = F,
-              weight = 1) %>%
+  addCircles(layerId = ~geometry,
+             fillColor = ~pal(distance),
+             fillOpacity = ~grid_opacity,
+             stroke = F,
+             weight = 1,
+             radius = 10^4) %>%
   addLegend("bottomright", pal = pal, values = ~distance,
             title = "Distance",
             opacity = 1)
