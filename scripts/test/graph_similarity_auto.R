a812018f34b121174bbb1c0f0ddb219614e903a2
Conor Tompkins <conorotompkins@gmail.com>
2022-01-17 13:39:15 -0500

    switched to binned distance, added test histograms

--- scripts/test/graph_similarity_auto.R
+++ scripts/test/graph_similarity_auto.R
@@ -3,6 +3,7 @@ library(sf)
 library(tigris)
 library(leaflet)
 library(mapdeck)
+library(ggspatial)
 
 set.seed(1234)
 
@@ -35,21 +36,55 @@ region_shape_moll %>%
 similarity_index <- read_csv("data/big/similarity_index.csv")
 
 similarity_geo <- similarity_index %>% 
-  prep_similarity_index(23)
+  prep_similarity_index(23) %>% 
+  mutate(month = fct_relevel(month, month.abb))
 
 similarity_geo %>% 
+  distinct(x, y, geo_index_compare) %>% 
   ggplot(aes(x, y, label = geo_index_compare)) +
   geom_text()
 
-library(ggspatial)
+bins <- c(-Inf, 0, seq(from = 10, to = 80, by = 10), Inf)
+length(bins)
 
-similarity_geo %>% 
+bin_labels <- c("0", "1-10", "10-20", "20-30", "30-40", "40-50", "50-60",
+                "60-70", "70-80", "80+")
+length(bin_labels)
+
+similarity_geo_binned <- similarity_geo %>% 
+  mutate(distance_bin = cut(distance, breaks = bins, labels = bin_labels))
+
+levels(similarity_geo_binned$distance_bin)
+
+similarity_geo_binned %>% 
+  filter(month == "Oct") %>% 
+  complete(distance_bin = bin_labels) %>% 
+  st_sf() %>% 
+  #View()
   ggplot() +
   annotation_map_tile(type = "stamenbw") + 
-  geom_sf(aes(fill = distance)) +
+  geom_sf(aes(fill = distance_bin)) +
   geom_sf(data = region_shape_moll, alpha = 0) +
-  geom_point(data = filter(similarity_geo, highlight_grid == T),
+  geom_point(data = filter(similarity_geo_binned, highlight_grid == T),
              aes(x, y), 
              color = "white") +
-  scale_fill_viridis_c(direction = 1) +
+  scale_fill_viridis_d(direction = 1) +
   labs(fill = "Distance")
+
+similarity_geo_binned %>% 
+  complete(month, distance_bin = bin_labels) %>% 
+  st_sf() %>% 
+  filter(month == "Oct") %>% 
+  filter(!is.na(distance)) %>% 
+  count(month, distance_bin) %>% 
+  ggplot(aes(distance_bin, n, fill = distance_bin)) +
+  geom_col() +
+  facet_wrap(~month, ncol = 2) +
+  scale_fill_viridis_d()
+
+similarity_geo_binned %>% 
+  ggplot(aes(distance, fill = distance_bin)) +
+  geom_histogram(binwidth = 10) +
+  geom_rug() +
+  facet_wrap(~month, ncol = 2, scales = "free") +
+  scale_fill_viridis_d()
