a5cd7a4df145397c0b5be07ebe464bf6f0d9199a
Conor Tompkins <conorotompkins@gmail.com>
2022-01-29 11:49:33 -0500

    created script to write abunds_table to csv

---
+++ scripts/test/save_abunds_table.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(lubridate)
+library(vroom)
+library(tools)
+
+#load abunds_table
+abunds_table <- list.files("data/big/species_abundance", full.names = T) %>% 
+  set_names() %>% 
+  map_dfr(vroom, delim = ",", .id = 'comName',
+          col_types = cols(
+            target_species = col_character(),
+            metric_desc = col_character(),
+            date = col_date(format = ""),
+            value = col_double(),
+            month = col_character(),
+            region = col_character(),
+            x = col_double(),
+            y = col_double()
+          )) %>% 
+  mutate(comName = basename(comName) %>% file_path_sans_ext,
+         month = month(date, label = T)) %>% 
+  mutate(x = round(x, 2),
+         y = round(y, 2)) %>% 
+  rename(abundance = value) %>% 
+  mutate(geo_id = str_c(x, y, sep = "_"))
+
+abunds_table %>% 
+  write_csv("data/big/abunds_table.csv")
