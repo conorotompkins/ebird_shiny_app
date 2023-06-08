d0d50bec51e173fab8ed270d381fb7c109c70c15
Conor Tompkins <conorotompkins@gmail.com>
2023-06-07 20:01:39 -0400

    improved code to transform and save full raster

--- scripts/processing/convert_raster_to_sf.R
+++ scripts/processing/convert_raster_to_sf.R
@@ -6,24 +6,36 @@ library(stars)
 
 path <- ebirdst_download(species = "Cape May Warbler", tifs_only = T, force = F)
 
-test <- load_raster(path = path, product = "abundance", period = "full-year", resolution = "lr")
+full_raster <- load_raster(path = path, product = "abundance", period = "full-year", resolution = "lr")
 
-test |> 
+raster_crs <- crs(full_raster)
+
+mollweide <- "+proj=moll +lon_0=-90 +x_0=0 +y_0=0 +ellps=WGS84"
+
+full_raster |> 
   plot()
 
-test |> 
+full_raster |> 
   class()
 
-test |> 
-  rast() |> 
-  vect()
-  st_as_sf()
+full_raster_transformed <- full_raster |> 
+  st_as_stars() |> 
+  st_as_sf() |> 
+  st_transform(mollweide) |> 
+  select(-1)
 
-st_read(path)
+full_raster_transformed |> 
+  plot()
 
-test |> 
+glimpse(full_raster_transformed)
+
+full_raster |> 
   st_as_stars() |> 
   st_as_sf() |> 
   rename(abundance = 1) |> 
   filter(abundance > 0) |> 
+  st_transform(mollweide) |> 
   plot()
+
+full_raster_transformed |> 
+  st_write("data/big/full_raster_transformed.shp")
